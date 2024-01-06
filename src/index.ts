3fbc225ed366b839605e50ad9ae538bf8e6a6205
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-06 07:27:01 -0300

    chore: bump version

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import type { MaybeArray, Nullish } from '@tb-dev/utility-types';
  *
  * Arrays and strings are considered empty if they have a length of 0.
  * Map and Sets are considered empty if they have a size of 0.
+ *
+ * Nullish items are always considered empty.
  */
 export function isEmpty(value?: Nullish<string>): boolean;
 export function isEmpty<T>(value?: Nullish<T[]>): boolean;
