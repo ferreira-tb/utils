aad880ad92563b673cd85f8612229a61c20d2daa
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-06 06:34:05 -0300

    chore: bump version

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,10 @@
 import config from '@tb-dev/eslint-config';
 
 export default config({
-  project: ['./tsconfig.json']
+  project: ['./tsconfig.json'],
+  overrides: {
+    typescript: {
+      '@typescript-eslint/explicit-function-return-type': 'error'
+    }
+  }
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
 import type { MaybeArray, Nullish } from '@tb-dev/utility-types';
 
+/**
+ * Check if a value is empty.
+ *
+ * Arrays and strings are considered empty if they have a length of 0.
+ * Map and Sets are considered empty if they have a size of 0.
+ */
+export function isEmpty(value?: Nullish<string>): boolean;
+export function isEmpty<T>(value?: Nullish<T[]>): boolean;
+export function isEmpty<K>(value?: Nullish<Set<K>>): boolean;
+export function isEmpty<K, V>(value?: Nullish<Map<K, V>>): boolean;
+export function isEmpty(value?: unknown): boolean {
+  if (Array.isArray(value) || typeof value === 'string') {
+    return value.length === 0;
+  }
+
+  if (value instanceof Map || value instanceof Set) {
+    return value.size === 0;
+  }
+
+  return true;
+}
+
 export function toArray<T>(item?: Nullish<MaybeArray<T>>): T[] {
   const array = item ?? [];
   return Array.isArray(array) ? array : [array];
