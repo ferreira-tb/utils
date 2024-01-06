0b6d10a368fb974aacb20d327ff94fdaa0300608
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-05 22:25:23 -0300

    chore: bump version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export {}
+import type { MaybeArray, Nullish } from '@tb-dev/utility-types';
+
+export function toArray<T>(item?: Nullish<MaybeArray<T>>): T[] {
+  const array = item ?? [];
+  return Array.isArray(array) ? array : [array];
+}
+
+/**
+ * Add the pixel unit to a value.
+ * If the value is a string, it is returned unchanged.
+ */
+export function toPixel(value: string | number): string {
+  if (typeof value === 'string') return value;
+  return `${value}px`;
+}
