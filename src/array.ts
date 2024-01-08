5728811e07f78c1475d83935ac89958e00847249
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-08 12:34:54 -0300

    chore: bump version

--- src/array.ts
+++ src/array.ts
@@ -1,6 +1,8 @@
 import type { MaybeArray, Nullish } from '@tb-dev/utility-types';
 
-export function splitWhitespace(value: string | string[]): string[] {
+export function splitWhitespace(value: Nullish<string | string[]>): string[] {
+  if (!value) return [];
+
   if (Array.isArray(value)) {
     const array = value.map((i) => splitWhitespace(i));
     return array.flat(Number.POSITIVE_INFINITY) as string[];
