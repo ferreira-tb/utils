2a375a38d02f2718e3023a2d7a63071b5d9720fa
Andrew Ferreira <andrew.shien2@gmail.com>
2024-03-29 15:09:25 -0300

    chore: bump version

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
 import { float as floatRegex } from './regex';
 
-export { splitWhitespace, toArray, trimArray, upsert } from './array';
+/**
+ * Check if a value is empty.
+ *
+ * Arrays and strings are considered empty if they have a length of 0.
+ * Map and Sets are considered empty if they have a size of 0.
+ *
+ * This function does not check for empty objects.
+ * Any other value is considered empty only if it is nullish.
+ */
+export function isEmpty(value?: unknown): boolean {
+  if (isNullish(value)) {
+    return true;
+  }
+
+  if (Array.isArray(value) || typeof value === 'string') {
+    return value.length === 0;
+  }
+
+  if (value instanceof Map || value instanceof Set) {
+    return value.size === 0;
+  }
+
+  return false;
+}
 
 export function isNullish(value: unknown): value is null | undefined {
   return value === null || value === undefined;
