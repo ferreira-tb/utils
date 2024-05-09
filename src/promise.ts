ced51721247f3634bf4c98f6b2354d2cb3836d59
Andrew Ferreira <andrew.shien2@gmail.com>
2024-05-09 00:19:44 -0300

    chore: bump version

--- src/promise.ts
+++ src/promise.ts
@@ -1,5 +1,7 @@
+const scheduler = typeof setImmediate === 'function' ? setImmediate : setTimeout;
+
 export function flush(): Promise<void> {
-  return new Promise((resolve) => void setTimeout(resolve, 0));
+  return new Promise((resolve) => void scheduler(resolve, 0));
 }
 
 export function sleep(ms: number): Promise<void> {
