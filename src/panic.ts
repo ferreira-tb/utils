1dfcb761fe8cb465a494a5c9649f62d34f6117d2
Andrew Ferreira <andrew.shien2@gmail.com>
2024-06-11 22:16:33 -0300

    chore: bump version

--- src/panic.ts
+++ src/panic.ts
@@ -1,15 +1,18 @@
-export function panic(message?: string): never {
-  throw new Error(message);
+export function panic(...args: any[]): never {
+  throw new Error(args[0]);
 }
 
-export function todo(message = 'not yet implemented'): never {
+export function todo(...args: any[]): never {
+  const message = typeof args[0] === 'string' ? args[0] : 'todo';
   panic(message);
 }
 
-export function unimplemented(message = 'not implemented'): never {
+export function unimplemented(...args: any[]): never {
+  const message = typeof args[0] === 'string' ? args[0] : 'not implemented';
   panic(message);
 }
 
-export function unreachable(message = 'unreachable'): never {
+export function unreachable(...args: any[]): never {
+  const message = typeof args[0] === 'string' ? args[0] : 'unreachable';
   panic(message);
 }
