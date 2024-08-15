273ea680c1ede86070f4e095d4fc2dc8bee610e4
Andrew Ferreira <andrew.shien2@gmail.com>
2024-08-14 21:15:07 -0300

    feat: add `Writeable` type

--- src/types.ts
+++ src/types.ts
@@ -40,6 +40,8 @@ export type PickPartial<T, K extends keyof T> = Pick<Partial<T>, K>;
 /** Constructs a type by picking the set of properties `K` from a required version of `T`. */
 export type PickRequired<T, K extends keyof T> = Pick<Required<T>, K>;
 
+export type Writeable<T> = { -readonly [P in keyof T]: T[P] };
+
 /** Constructs a type consisting of some properties of T set to partial. */
 export type WithPartial<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
